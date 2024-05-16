d5eaeb4760bb610eabe07acfb5925afee955d6b4
Kaloyan Borisov <kaloyan.borisov@turbulence.tech>
2024-05-16 15:13:38 +0300

    First proper training

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8m.yaml")  # build a new model from scratch
 
 # Use the model
 model.train(data="trainer_config.yaml", epochs=100)  # train the model

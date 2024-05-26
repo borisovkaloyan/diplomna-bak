cda7118cee674a765ec768b420a2f7cee9cccc46
borisovkaloyan <borisovkaloyan@gmail.com>
2024-05-26 20:11:19 +0300

    Add maui app

---
+++ ParkingBuddy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkingBuddy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

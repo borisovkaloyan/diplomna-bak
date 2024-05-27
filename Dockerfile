2618bdf17b65f3f207d7e665dce0bc14fdbad4fe
Kaloyan Borisov <kaloyan.borisov@turbulence.tech>
2024-05-27 13:39:38 +0300

    Add google cloud support

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11.9-alpine3.20
+
+WORKDIR /app
+COPY dist/pariking_buddy_backend-1.0.0-py3-none-any.whl /app
+
+RUN apk --update --no-cache add make automake gcc g++ subversion python3-dev python3
+RUN pip install pariking_buddy_backend-1.0.0-py3-none-any.whl
+
+EXPOSE 8000
+
+CMD ["parking-buddy"]
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Setup package
+setup(
+    name="pariking-buddy-backend",   # App name
+    version="1.0.0",                 # App version
+    packages=find_packages(),   # App packages
+    install_requires=[          # App requirements
+        "fastapi",
+        "sqlalchemy",
+        "psycopg2-binary"
+    ],
+    entry_points={          # App entry point
+        'console_scripts': [
+            'parking-buddy = backend.__main__:backend_entrypoint',
+        ],
+    },
+    include_package_data=True
+)

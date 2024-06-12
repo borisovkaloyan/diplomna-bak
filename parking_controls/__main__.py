92f8dfa1f4c3d0053b43e75e6b6d3924987dba79
Kaloyan Borisov <kaloyan.borisov@turbulence.tech>
2024-06-12 09:42:19 +0300

    resize image for ocr

--- parking_controls/__main__.py
+++ parking_controls/__main__.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+import time
 from ultralytics import YOLO
 
 import cv2
@@ -8,17 +9,26 @@ import parking_controls.utils as utils
 from parking_controls.backend_client import Client
 from parking_controls.backend_client.types import Response
 
-# load backend client
+# Load backend client
 client = Client("http://34.118.78.253")
 
-# store results here
+# Store results here
 results = {}
 
-# load models
-# license_plate_detector = YOLO('./parking_controls/plate_model.pt')
-license_plate_detector = YOLO('./parking_controls/plate_model_light.pt')
+# Config
+nano = True
+resize = True
+active_backend = True
 
-# load video
+# Load models
+if nano:
+    license_plate_detector = YOLO('./parking_controls/plate_model_light.pt')
+    min_confidence = 0.2
+else:
+    license_plate_detector = YOLO('./parking_controls/plate_model.pt')
+    min_confidence = 0.3
+
+# Load video
 cap = cv2.VideoCapture('./parking_controls/test_video.mp4')
 
 # Frame management
@@ -40,20 +50,24 @@ with client as client:
         ret, frame = cap.read()
         if ret:
             results[frame_nmr] = {}
-            detections = license_plate_detector(frame)[0]
+            detections = license_plate_detector(frame, iou=0.1)[0]
 
             for plate in detections.boxes.data.tolist():
                 x1, y1, x2, y2, score, _ = plate
-                # Change to 0.3 for m model
-                if score > 0.2:
+
+                if score > min_confidence:
 
                     license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
+                    if resize:
+                        license_plate_crop = cv2.resize(license_plate_crop, (150, 40))
+
                     license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
                     _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
 
                     plate_height, plate_width = license_plate_crop_thresh.shape
 
                     license_plate_text, license_plate_text_score = utils.read_license_plate(license_plate_crop_thresh)
+
                     print(f"Text: {license_plate_text} Score: {license_plate_text_score}")
 
                     if license_plate_text_score and license_plate_text_score > best_score:
@@ -64,13 +78,14 @@ with client as client:
 
                     if best_score > 0.8 and try_backend:
                         try_backend = False
-                        response: Response = utils.call_backend(client, best_text)
-                        print(f"Backend response: {response}")
-                        if (response.status_code == HTTPStatus.CREATED) or (response.status_code == HTTPStatus.OK):
-                            door_message = "Barrier lifted!"
-                        else:
-                            door_message = "Barrier closed!"
-                        backend_message = "Backend response: " + str(json.loads(response.content.decode("utf-8"))["message"])
+                        if active_backend:
+                            response: Response = utils.call_backend(client, best_text)
+                            print(f"Backend response: {response}")
+                            if (response.status_code == HTTPStatus.CREATED) or (response.status_code == HTTPStatus.OK):
+                                door_message = "Barrier lifted!"
+                            else:
+                                door_message = "Barrier closed!"
+                            backend_message = "Backend response: " + str(json.loads(response.content.decode("utf-8"))["message"])
 
                     if backend_message:
                         cv2.putText(
